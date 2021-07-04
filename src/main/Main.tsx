451ab88a37a665c43eb41e28dec6b36283f122a8
shobanov <shobanoff@gmail.com>
2021-07-04 12:02:55 +0300

    added typing in the main block

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,4 +1,5 @@
 import s from './Main.module.css'
+import { ReactTypical } from '@deadcoder0904/react-typical'
 
 function Main() {
 
@@ -8,7 +9,12 @@ function Main() {
         <div className={s.text}>
           <span>Hi there</span>
           <h1>I am Borat</h1>
-          <p>Frontend Developer</p>
+          <p><ReactTypical
+                steps={['Frontend Developer', 2000, 'and just a good guy 👍', 2000]}
+                loop={Infinity}
+                wrapper="p"
+              />
+          </p>
         </div>
         <div className={s.foto}><img src='https://media1.tenor.com/images/c52ad4746e8b45e15e91256d67830772/tenor.gif?itemid=18652331' width="100%" height="auto" alt="gif" /></div>
       </div>
